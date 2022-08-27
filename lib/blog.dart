cbc3e8782b3f6945647f3abb2d32049206a05f22
Roeman1505 <robinsonemmanuel1505@gmail.com>
2022-08-28 00:31:59 +0300

    adding indigrate

--- lib/blog.dart
+++ lib/blog.dart
@@ -4,38 +4,72 @@ class Blog {
   // ignore: todo
   //TODO:Add Servings and Ingredients here
 
+  int servings;
+  List<Ingredient> ingredients;
+
   Blog(
     this.label,
     this.imageUrl,
+    this.servings,
+    this.ingredients,
   );
   // ignore: todo
   //TODO: Add List<Recipe> here
-  
-  }
-    // ignore: todo
-  //TODO: Add Ingredient() here
-  class Ingredient{
-  double quantity;
-    String measure;
-    String name;
-    Ingredient(
-      this.quantity,
-      this.measure,
-      this.name,
-    );
 
-
-
-
-  }
-  
- 
   static List<Blog> samples = [
-    Blog('Spaghetti and Meatballs', 'assets/images/food1.jpg'),
-    Blog('Tomato Soup', 'assets/images/food2.jpg'),
-    Blog('Grilled Cheese', 'assets/images/food3.jpg'),
-    Blog('Chocolate Chips Cookies', 'assets/images/food4.jpg'),
-    Blog('Taco Salad', 'assets/images/food5.jpeg'),
-    Blog('Hawaii Pizza', 'assets/images/food6.jpeg'),
+    Blog('Spaghetti and Meatballs', 'assets/images/food1.jpg', 4,[
+      Ingredient(1, 'box', 'Spaghetti',),
+      Ingredient(4, '', 'Frozen Meatballs',),
+      Ingredient(0.5, 'jar', 'Sauce',),
+
+      
+    ]),
+    Blog('Tomato Soup', 'assets/images/food2.jpg',  2,
+    [
+      Ingredient(1, 'can', 'Tomato Soup',),
+    ]
+    
+    
+    ),
+    Blog('Grilled Cheese', 'assets/images/food3.jpg', 1, 
+    [
+      Ingredient(2, 'slice', 'Cheese',),
+      Ingredient(2, 'slice', 'Bread',),
+    ]),
+    Blog('Chocolate Chips Cookies', 'assets/images/food4.jpg',24,
+    [
+      Ingredient(4, 'cups', 'Floor',),
+      Ingredient(2, 'cups', 'sugar',),
+      Ingredient(0.5, 'cups', 'Chocolate chips',),
+    ]
+    ),
+    Blog('Taco Salad', 'assets/images/food5.jpeg',1,
+    [
+Ingredient(4, 'oz', 'nachos',),
+Ingredient(3, 'oz', 'taco Meat',),
+Ingredient(0.5, 'cup', 'cheese',),
+Ingredient(0.25, 'cup', 'Chopped tomatoes',),
+
+
+    ]),
+    Blog('Hawaii Pizza', 'assets/images/food6.jpeg',4,
+    [
+     Ingredient(1, 'item', 'Pizza',),
+     Ingredient(1, 'cup', 'pineapple',),
+     Ingredient(8, 'oz', 'ham',),
+    ]),
   ];
+}
+
+// ignore: todo
+//TODO: Add Ingredient() here
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+  Ingredient(
+    this.quantity,
+    this.measure,
+    this.name,
+  );
 }
--- lib/blog_details.dart
+++ lib/blog_details.dart
@@ -23,40 +23,44 @@ class _BlogDetailState extends State<BlogDetail> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      title: Text(widget.blog.label),
-    ),
-    body: SafeArea(
-      child:Column(
-        children:<Widget>[
-           SizedBox(
-            height:300,
-            width:double.infinity,
-            child:Image(
-              image: AssetImage(widget.blog.imageUrl),
-            ),
-           ),
-           const SizedBox(
-            height: 4,
-           ),
-         Text(
-          widget.blog.label,
-          style: const TextStyle(fontSize:18)
+          title: Text(widget.blog.label),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image(
+                  image: AssetImage(widget.blog.imageUrl),
+                ),
+              ),
+              const SizedBox(
+                height: 4,
+              ),
+              Text(widget.blog.label, style: const TextStyle(fontSize: 18)),
+              // ignore: todo
+              //TODO: Add Expanded
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(7.0),
+                  itemCount: widget.blog.ingredients.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final ingredient = widget.blog.ingredients[index];
 
-         )
-     // ignore: todo
-     //TODO: Add Expanded
+                    // ignore: todo
+                    //TODO:Add ingredient.quantity
 
-     // ignore: todo
-     //TODO: Add Slider() here
-        ],
-      ),
+                    return Text(
+                        '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                  },
+                ),
+              ),
 
-
-    )
-    
-    
-    
-    
-    );
+              // ignore: todo
+              //TODO: Add Slider() here
+            ],
+          ),
+        ));
   }
 }
