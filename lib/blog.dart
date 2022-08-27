cdf5d4e419b3d199e3a7bb293131d564c2a4451d
Roeman1505 <robinsonemmanuel1505@gmail.com>
2022-08-27 18:30:11 +0300

    First Flutter app

---
+++ lib/blog.dart
@@ -0,0 +1,22 @@
+class Blog {
+  String label;
+  String imageUrl;
+  // ignore: todo
+  //TODO:Add Servings and Ingredients here
+
+  Blog(
+    this.label,
+    this.imageUrl,
+  );
+  // ignore: todo
+  //TODO: Add List<Recipe> here
+
+  static List<Blog> samples = [
+    Blog('Spaghetti and Meatballs', 'assets/images/food1.jpg'),
+    Blog('Tomato Soup', 'assets/images/food2.jpg'),
+    Blog('Grilled Cheese', 'assets/images/food3.jpg'),
+    Blog('Chocolate Chips Cookies', 'assets/images/food4.jpg'),
+    Blog('Taco Salad', 'assets/images/food5.jpeg'),
+    Blog('Hawaii Pizza', 'assets/images/food6.jpeg'),
+  ];
+}
